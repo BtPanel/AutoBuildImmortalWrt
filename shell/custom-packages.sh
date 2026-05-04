c22016bd04fe88093fdbed5cf0f87daf8d96eb33
BtPanel <Uler365@gmail.com>
2026-05-04 17:50:47 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -10,7 +10,6 @@
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES quickstart luci-app-quickstart luci-i18n-quickstart-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES ddnsto app-meta-ddnsto luci-app-ddnsto luci-i18n-ddnsto-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES gowebdav luci-app-gowebdav app-meta-gowebdav luci-i18n-gowebdav-zh-cn"
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES linkease app-meta-linkease luci-app-linkease luci-i18n-linkease-zh-cn"
 
 # 高级卸载 by YT Vedio Talk
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
