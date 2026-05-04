45d9a8461aa95f9fee4c4cf7e8a7529c00723f99
BtPanel <Uler365@gmail.com>
2026-05-04 12:17:29 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -47,7 +47,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES iptables-nft xtables-nft libiptext-nft0 kmod-nft-compat "
+PACKAGES="$PACKAGES iptables-nft xtables-nft libiptext-nft0 kmod-nft-compat"
 PACKAGES="$PACKAGES ip6tables-nft"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
